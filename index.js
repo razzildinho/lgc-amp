731866510fc69f64bdf3cc06fc1311f8f8ba6e2e
Ronan Treacy <hello@ronantreacy.com>
2018-05-09 16:32:12 +0100

    added sti amp blank

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const proxy = require('express-http-proxy');
 
 app.get('/', (req, res) => res.sendFile('./index.html', {root: __dirname}))
 app.get('/index-amp', (req, res) => res.sendFile('./index.amp.html', {root: __dirname}))
+app.get('/sti-amp', (req, res) => res.sendFile('./sti-amp.html', {root: __dirname}))
 app.use('/index_files', express.static('index_files'));
 app.use('/assets', express.static('assets'));
 
