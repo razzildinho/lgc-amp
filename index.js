ee694587b2b92feed4d48ff760fb0d951bbb7863
Ronan Treacy <hello@ronantreacy.com>
2018-05-09 10:51:47 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const proxy = require('express-http-proxy');
+
+app.get('/', (req, res) => res.sendFile('./index.html', {root: __dirname}))
+app.get('/index-amp', (req, res) => res.sendFile('./index.amp.html', {root: __dirname}))
+app.use('/index_files', express.static('index_files'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
