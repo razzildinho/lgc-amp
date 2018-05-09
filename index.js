9650be7b9c22db2e431189c47f31b4dd32eb73c2
Ronan Treacy <hello@ronantreacy.com>
2018-05-09 11:20:05 +0100

    changed to sti page

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const proxy = require('express-http-proxy');
 app.get('/', (req, res) => res.sendFile('./index.html', {root: __dirname}))
 app.get('/index-amp', (req, res) => res.sendFile('./index.amp.html', {root: __dirname}))
 app.use('/index_files', express.static('index_files'));
+app.use('/assets', express.static('assets'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
